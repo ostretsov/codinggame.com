cb2c25890b09f06cc8fa0f4d3977b76a8b54b6eb
Artem Ostretsov <OstretsovAA@gmail.com>
2019-05-17 11:02:54 +0300

    Intermediate BFS implementation of https://www.codingame.com/training/medium/skynet-revolution-episode-1

--- skynet/skynet.go
+++ skynet/skynet.go
@@ -3,6 +3,7 @@ package skynet
 import (
 	"errors"
 	"fmt"
+	"io"
 )
 
 type node int
@@ -23,9 +24,9 @@ func (l link) getLinked(n node) (node, error) {
 	return 0, errors.New("Invalid link")
 }
 
-//func (l link) isSame(targetLink link) bool {
-//	return (targetLink.n1 == l.n1 && targetLink.n2 == l.n2) || (targetLink.n2 == l.n1 && targetLink.n1 == l.n2)
-//}
+func (l link) isEqual(targetLink *link) bool {
+	return (targetLink.n1 == l.n1 && targetLink.n2 == l.n2) || (targetLink.n2 == l.n1 && targetLink.n1 == l.n2)
+}
 
 type graph struct {
 	links []link
@@ -54,46 +55,93 @@ type treeNode struct {
 	parentTreeNode *treeNode
 }
 
-//func (g *graph) unlink(targetLink link) {
-//	for i, l := range g.links {
-//		if (l.isSame(targetLink)) {
-//			g.links = append(g.links[:i])
-//		}
-//	}
-//}
-
-func solve() {
-	var skynetNode node = 0
-	var virusNode node = 3
-	g := graph{
-		[]link{
-			{0, 1},
-			{0, 2},
-			{1, 3},
-			{2, 3},
-		},
+func (t treeNode) getRootLink() (*link, error) {
+	if t.parentTreeNode == nil {
+		return nil, errors.New("Root treeNode does not have a parent!")
 	}
 
-	visitedNodes := []node{virusNode}
-	queue := make(chan treeNode, 500)
-	rootTreeNode := treeNode{virusNode, nil}
-	queue <- rootTreeNode
+	currentTreeNode := t
+	parentTreeNode := currentTreeNode.parentTreeNode
+	for parentTreeNode.parentTreeNode != nil {
+		currentTreeNode = *parentTreeNode
+		parentTreeNode = currentTreeNode.parentTreeNode
+	}
 
-	var foundDstTreeNode treeNode
-	for currentTreeNode := range queue {
-		if currentTreeNode.payload == skynetNode {
-			foundDstTreeNode = currentTreeNode
-			close(queue)
+	return &link{parentTreeNode.payload, currentTreeNode.payload}, nil
+}
+
+func (g *graph) unlink(targetLink *link) {
+	for i, l := range g.links {
+		if l.isEqual(targetLink) {
+			g.links = append(g.links[:i], g.links[i+1:]...)
 			break
 		}
+	}
+}
 
-		linkedNotVisitedNodes := g.getLinkedNodes(currentTreeNode.payload, visitedNodes)
-		for _, l := range linkedNotVisitedNodes {
-			s := currentTreeNode
-			queue <- treeNode{l, &s}
-			visitedNodes = append(visitedNodes, l)
-		}
+func solve(in io.Reader, out io.Writer) {
+	var nodeCount, linkCount, gwCount int
+	fmt.Scanf("%d %d %d\n", &nodeCount, &linkCount, &gwCount)
+	links := []link{}
+	for i := 0; i < linkCount; i++ {
+		var n1, n2 node
+		fmt.Scanf("%d %d\n", &n1, &n2)
+		links := append(links, link{n1, n2})
+	}
+	gateways := []node{}
+	for i := 0; i < gwCount; i++ {
+		var gw node
+		fmt.Scanf("%d\n", &gw)
+		gateways = append(gateways, gw)
 	}
 
-	fmt.Printf("%#v", foundDstTreeNode)
+	g := graph{links}
+
+	var skynetNode node
+	for {
+		fmt.Scanf("%d\n", &skynetNode)
+
+		closestTreeNodes := []treeNode{}
+
+		for _, gw := range gateways {
+			virusNode := gw
+			visitedNodes := []node{virusNode}
+			queue := make(chan treeNode, 500)
+			rootTreeNode := treeNode{virusNode, nil}
+			queue <- rootTreeNode
+
+			var foundDstTreeNode treeNode
+			for currentTreeNode := range queue {
+				if currentTreeNode.payload == skynetNode {
+					foundDstTreeNode = currentTreeNode
+					close(queue)
+					break
+				}
+
+				linkedNotVisitedNodes := g.getLinkedNodes(currentTreeNode.payload, visitedNodes)
+				for _, l := range linkedNotVisitedNodes {
+					s := currentTreeNode
+					queue <- treeNode{l, &s}
+					visitedNodes = append(visitedNodes, l)
+				}
+			}
+
+			closestTreeNodes = append(closestTreeNodes, foundDstTreeNode)
+		}
+
+		// sort closesTreeNodes
+		// get closest to gw
+		// get root link
+		// sever the link
+
+		//fmt.Printf("Found dst tree node: %v\n", foundDstTreeNode)
+		//
+		//rootLink, err := foundDstTreeNode.getRootLink()
+		//if err != nil {
+		//	log.Fatal(err)
+		//}
+		//fmt.Printf("Root link: %v", rootLink)
+		//
+		//g.unlink(rootLink)
+	}
 }
--- skynet/skynet_test.go
+++ skynet/skynet_test.go
@@ -1,7 +1,24 @@
 package skynet
 
-import "testing"
+import (
+	"coding-game"
+	"testing"
+)
 
 func TestSolve(t *testing.T) {
-	solve()
+	var testCases = []coding_game.TestCase{
+		{
+			"3 nodes, 1 gw",
+			`3 2 1
+0 1
+1 2
+2
+0`,
+			`2 1`,
+		},
+	}
+
+	for _, tc := range testCases {
+		coding_game.TestSolve(t, solve, tc)
+	}
 }
